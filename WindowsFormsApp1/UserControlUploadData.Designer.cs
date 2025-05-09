fa17df18e6c8dc68d6dbabdb7c8dc174286de259
feketealexm <feketealexm@gmail.com>
2025-05-09 09:54:27 +0200

    Kisebb dizájn javítások

--- WindowsFormsApp1/UserControlUploadData.Designer.cs
+++ WindowsFormsApp1/UserControlUploadData.Designer.cs
@@ -39,20 +39,21 @@
             // 
             // pictureBoxSuccess
             // 
+            this.pictureBoxSuccess.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBoxSuccess.Image = global::WindowsFormsApp1.Properties.Resources.uploadsuccess;
-            this.pictureBoxSuccess.Location = new System.Drawing.Point(29, 22);
+            this.pictureBoxSuccess.Location = new System.Drawing.Point(136, 22);
             this.pictureBoxSuccess.Name = "pictureBoxSuccess";
-            this.pictureBoxSuccess.Size = new System.Drawing.Size(1008, 452);
-            this.pictureBoxSuccess.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxSuccess.Size = new System.Drawing.Size(901, 452);
+            this.pictureBoxSuccess.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxSuccess.TabIndex = 0;
             this.pictureBoxSuccess.TabStop = false;
             // 
             // pictureBoxDragAndDrop
             // 
             this.pictureBoxDragAndDrop.Image = global::WindowsFormsApp1.Properties.Resources.dragndrop;
-            this.pictureBoxDragAndDrop.Location = new System.Drawing.Point(29, 22);
+            this.pictureBoxDragAndDrop.Location = new System.Drawing.Point(136, 22);
             this.pictureBoxDragAndDrop.Name = "pictureBoxDragAndDrop";
-            this.pictureBoxDragAndDrop.Size = new System.Drawing.Size(1008, 452);
+            this.pictureBoxDragAndDrop.Size = new System.Drawing.Size(901, 452);
             this.pictureBoxDragAndDrop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxDragAndDrop.TabIndex = 1;
             this.pictureBoxDragAndDrop.TabStop = false;
@@ -60,7 +61,7 @@
             // buttonEnd
             // 
             this.buttonEnd.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.buttonEnd.Location = new System.Drawing.Point(431, 500);
+            this.buttonEnd.Location = new System.Drawing.Point(499, 480);
             this.buttonEnd.Name = "buttonEnd";
             this.buttonEnd.Size = new System.Drawing.Size(208, 80);
             this.buttonEnd.TabIndex = 2;
@@ -70,7 +71,7 @@
             // 
             // buttonUpload
             // 
-            this.buttonUpload.Location = new System.Drawing.Point(558, 450);
+            this.buttonUpload.Location = new System.Drawing.Point(614, 422);
             this.buttonUpload.Name = "buttonUpload";
             this.buttonUpload.Size = new System.Drawing.Size(111, 32);
             this.buttonUpload.TabIndex = 3;
@@ -81,7 +82,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(394, 458);
+            this.label1.Location = new System.Drawing.Point(450, 430);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(140, 16);
             this.label1.TabIndex = 4;
